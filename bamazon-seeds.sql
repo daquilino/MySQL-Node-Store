7df68169623dd05c0f52aad06f22edac0b736360
Douglas Aquilino <douglas_aquilino@yahoo.com>
2017-04-12 16:29:20 -0400

    minor tweeks

--- bamazon-seeds.sql
+++ bamazon-seeds.sql
@@ -1,28 +1,28 @@
 USE Bamazon_db;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product1", "toys", 12.99, 93);
+VALUES ("Lego Police Station", "toys", 37.99, 93);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product2", "toys", 65.99, 3);
+VALUES ("Jacks", "toys", 5.99, 25);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product3", "computers", 55.99, 72);
+VALUES ("Apple ipod", "computers", 148.99, 72);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product4", "computers", 45.99, 48);
+VALUES ("Moon Rocks", "space", 1545.99, 48);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product5", "video games", 35.99, 7);
+VALUES ("John Doe Costume", "costumes", 35.99, 7);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product6", "video games", 15.99, 35);
+VALUES ("ROBLOX", "video games", 15.99, 35);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("bowler hat", "haberdashery", 25.99, 25);
+VALUES ("Bowler Hat", "haberdashery", 25.99, 25);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("product8", "toys", 5.99, 93);
+VALUES ("Jelly Beans", "candy", 2.99, 93);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Death Ray", "Super Villian Weapons", 99999999.99 , 1);
