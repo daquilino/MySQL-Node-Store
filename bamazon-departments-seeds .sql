937fd2fa80fb0b1df4d86050fc6785b789548f44
Douglas Aquilino <douglas_aquilino@yahoo.com>
2017-04-14 12:31:25 -0400

    modified bamazon-departments-seeds.sql

--- bamazon-departments-seeds .sql
+++ bamazon-departments-seeds .sql
@@ -1,14 +1,14 @@
 USE Bamazon_db;
 
-INSERT INTO departmentss (department_name, over_head_costs)
-VALUES ("Toys", 500);
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("Toys", 1100);
 
-INSERT INTO departmentss (department_name, over_head_costs)
+INSERT INTO departments (department_name, over_head_costs)
 VALUES ("Electronics", 3500);
 
-INSERT INTO departmentss (department_name, over_head_costs)
+INSERT INTO departments (department_name, over_head_costs)
 VALUES ("Haberdashery", 1500);
 
-INSERT INTO departmentss (department_name, over_head_costs)
+INSERT INTO departments (department_name, over_head_costs)
 VALUES ("Super Villian Weapons", 1000000);
 
