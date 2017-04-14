a1c1faa16624a89ab3c8760ad7568b7712faf099
Douglas Aquilino <douglas_aquilino@yahoo.com>
2017-04-14 11:01:57 -0400

    created bamazonSupervisor.js, modified bamazon-products-seeds.sql

--- bamazon-products-seeds.sql
+++ bamazon-products-seeds.sql
@@ -1,31 +1,31 @@
 USE Bamazon_db;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Lego Police Station", "toys", 37.99, 93);
+VALUES ("Lego Police Station", "toys", 37.99, 27);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Jacks", "toys", 5.99, 25);
+VALUES ("Jacks", "toys", 5.99, 250);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Apple ipod", "computers", 148.99, 72);
+VALUES ("Etch-A-Sketch", "toys", 15.99, 48);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Moon Rocks", "space", 1545.99, 48);
+VALUES ("Apple ipod", "electronics", 148.99, 72);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("John Doe Costume", "costumes", 35.99, 7);
+VALUES ("Screen Wipes", "electronics", 1.99, 354);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("ROBLOX", "video games", 15.99, 35);
+VALUES ("Earbuds", "electronics", 12.99, 108);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Bowler Hat", "Haberdashery", 25.99, 25);
+VALUES ("Bowler Hat", "haberdashery", 25.99, 32);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Jelly Beans", "candy", 2.99, 93);
+VALUES ("Argyle Socks", "haberdashery", 7.99, 158);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Death Ray", "Super Villian Weapons", 100000000.00 , 1);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Shrink Ray", "Super Villian Weapons", 689.99, 52);
+VALUES ("Shrink Ray", "Super Villian Weapons", 689999.99, 5);
